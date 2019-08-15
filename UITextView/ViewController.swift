b3f58d5ae0a38cbbac9c54b49dfbc5a25e96aa7c
SergeyGolenko <1989sergiotay@gmail.com>
2019-08-15 13:52:24 +0300

    add touchesBegan

--- UITextView/ViewController.swift
+++ UITextView/ViewController.swift
@@ -14,14 +14,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       myTextView = UITextView(frame: self.view.bounds)
+     createTextView()
+    }
+   
+    func createTextView(){
+        myTextView = UITextView(frame: .init(x: 20, y: 100, width: self.view.bounds.width - 40, height: self.view.bounds.height / 2))
         myTextView.text = "Some text"
         myTextView.contentInset = UIEdgeInsets(top:  40, left: 0, bottom: 0, right: 0)
         myTextView.font = UIFont.systemFont(ofSize: 16)
         myTextView.backgroundColor = UIColor.purple
         self.view.addSubview(myTextView)
+        
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.myTextView.resignFirstResponder()
+    }
+    
 
 }
 
